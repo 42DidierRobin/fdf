6b94d07cd26045633c724c98ec938fb2db8ca923
Robin DIDIER <rdidier@e3r8p21.42.fr>
2016-01-19 16:46:44 +0100

    La sauvegarde parano

--- sources/main.c
+++ sources/main.c
@@ -6,17 +6,41 @@
 /*   By: rdidier <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/19 15:15:02 by rdidier           #+#    #+#             */
-/*   Updated: 2016/01/19 15:30:03 by rdidier          ###   ########.fr       */
+/*   Updated: 2016/01/19 16:46:13 by rdidier          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/file_de_fer.h"
 
-int		main(void)
+//TEMP
+void		print_grid(t_grid **grid)
 {
-	char	*yo;
+	int			i;
+	int			j;
 
-	ft_putstr("yoo\n");
-	ft_putnbr(get_next_line(3, &yo));
+	i = 0;
+	ft_putendl("Tableau enregistre :");
+	while (i < (*grid)->height)
+	{
+		j = 0;
+		while (j < (*grid)->length)
+		{
+			ft_putnbr((*grid)->tab[i][j]);
+			ft_putchar('-');
+		}
+		ft_putchar('\n');
+	}
+	ft_putstr("fin du tableau\n");
+	ft_putchar('\n');
+}
+//TEMP
+
+int		main(int argc, char **argv)
+{
+	t_grid		*grid;
+
+	argc = argc;
+	grid = reat_it(argv[1]);
+	print_grid(&grid);
 	return (0);
 }
--- sources/read_stuff.c
+++ sources/read_stuff.c
@@ -6,10 +6,31 @@
 /*   By: rdidier <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/19 15:42:55 by rdidier           #+#    #+#             */
-/*   Updated: 2016/01/19 15:48:48 by rdidier          ###   ########.fr       */
+/*   Updated: 2016/01/19 16:46:30 by rdidier          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/file_de_fer"
 
+static void			char_to_int(t_grid **grid)
+{
 
+}
+
+t_grid				*read_it(char *file_name)
+{
+	t_grid		*ret;
+	int			fd;
+	int			gnl_ret;
+	char		**tab;
+
+	i = 0;
+	ret = (t_grid*)malloc(sizeof(t_grid));
+	char = (char**)malloc(sizeof(char*) * SIZE_MAX);
+	fd = open(file_name, O_RDONLY);
+	while ((gnl_ret = get_next_line(fd, &(tab[i]))))
+		i++;
+	ret->height = i;
+	char_to_int_tab(&ret);
+	return (ret);
+}
